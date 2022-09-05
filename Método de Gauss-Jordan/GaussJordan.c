43eaca959f11d2479c5d959614fed4b51253a149
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-09-05 18:57:08 -0300

    update

--- Método de Gauss-Jordan/GaussJordan.c
+++ Método de Gauss-Jordan/GaussJordan.c
@@ -1,38 +1,91 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void mulLine(int rows, int cols, int resultLine, int mult, int matrix[rows][cols]){
-    for(int i = 0; i < cols; i++) matrix[resultLine][i] = mult*matrix[resultLine][i]; 
+void printMatrix(float **matrix, int rows, int cols){
+    for(int i = 0; i < rows; i++){ 
+        for(int j = 0; j < cols; j++){
+            printf("%f ", matrix[i][j]);
+        }
+        printf("\n");
+    }
 }
 
-void addLine(int rows, int cols, int resultLine, int sumLine, int sumLineMul, int matrix[rows][cols]){
+void mulLine(float **matrix, int cols, int resultLine, float mult){
+    for(int i = 0; i < cols; i++) matrix[resultLine][i] = matrix[resultLine][i]*mult; 
+}
+
+void divLine(float **matrix, int cols, int resultLine, float divisor){
+    for(int i = 0; i < cols; i++) matrix[resultLine][i] = matrix[resultLine][i]/divisor; 
+}
+
+void addLine(float **matrix, int cols, int resultLine, int sumLine, float sumLineMul){
     for(int i = 0; i < cols; i++) matrix[resultLine][i] = matrix[resultLine][i] + sumLineMul*matrix[sumLine][i];
 }
 
-
-void gaussJordan(int rows, int cols, int matrix[rows][cols]){
-    for(int i = 0; i < cols; i++) for(int j = 0; j < rows; j++){ // Andar pelas colunas e em cada coluna andar pelas linhas
-            if(matrix[i][j] != 0){
-                    for(int k = 0; k < rows; k++){ // Quando achar a primeira linha diferente de 0:
-                        if(k == j && matrix[i][k] == 1) continue;
-                        else if(k == j && matrix[i][k] != 0){
-                            mulLine(rows, cols, k, 1/matrix[i][k], matrix);
-                            continue;
-                        }
-                        addLine(rows, cols, k, j, -(matrix[i][k])/matrix[i][j], matrix);                    
-                }
-            }
+void swapLine(float **matrix, int cols, int Line1, int Line2){
+    float temp;
+    for(int i = 0; i < cols; i++){
+        temp = matrix[Line1][i];
+        matrix[Line1][i] = matrix[Line2][i];
+        matrix[Line2][i] = temp;
     }
 }
 
-void printMatrix(int rows, int cols, int matrix[rows][cols]){
-    for(int i = 0; i < rows; i++) for(int j = 0; j < cols; j++){
-        printf("%i ", matrix[i][j]);
-        if(j = cols-1) printf("\n");
+// matriz com algum coeficiente 0 não está funcionando (sugestão: fazer função que troque as linhas)
+
+void gaussJordan(float **matrix, int rows, int cols){
+    cols = cols -1; // a última coluna é a matriz B tal que A.X = B, ou seja, valores que o sistema de equações deve satisfazer
+    for(int j = 0; j < cols; j++){
+        for(int i = j; i < rows; i++){
+            if(matrix[i][j] != 0){
+                printf("dividir linha %i (pivo %f)\n", i, matrix[i][j]);
+                divLine(matrix, cols, i, matrix[i][j]);
+                for(int k = 0; k < rows; k++){
+                    if(k != i) addLine(matrix, cols, k, i, -matrix[k][j]);
+                }
+                printf("\n");
+                printMatrix(matrix, rows, cols);
+                printf("\n");
+                break;
+            }
+        }
     }
+}
+
+float **createMatrix(int *rows, int *cols){
+
+    FILE *filePointer;
+    filePointer = fopen("Matrix.txt", "r");
+    fscanf(filePointer, "%i %i\n", rows, cols); 
+    printf("%i %i \n", *rows, *cols);
+
+    // (1) Alocando os ponteiros para as linhas da matriz:
+    // - o ponteiro de ponteiro aponta para um espaço a partir de onde há um número rows de ponteiros para int's
+    float **matrix = (float **)malloc((*rows)*sizeof(float*));
+
+    // (2) Alocando os ponteiros para as colunas da matriz:
+    // - cada ponteiro para int aponta para um espaço a partir de onde há um número columns de int's
+    for(int i=0; i<(*rows); i++){
+        matrix[i] = (float *)malloc((*cols)*sizeof(float));
+
+        // (2.1) Setando os elementos da matriz iguais à zero para inserção (por meio de adição) das arestas na função createGraph:
+        for(int j=0; j<(*cols); j++) fscanf(filePointer, "%f ", &matrix[i][j]);
+        //fscanf(filePointer, "\n");
+    }
+    return matrix;
 }
 
 int main(){
-    int matrix[3][3] = {{1, 2, 4}, {5, 1, 3}};
-    gaussJordan(3, 3, matrix);
+    int *rows;
+    rows = malloc(sizeof(int));
+    int *cols;
+    cols = malloc(sizeof(int));
+    
+    float **matrix = createMatrix(rows, cols);
+    printMatrix(matrix, *rows, *cols);
+    gaussJordan(matrix, *rows, *cols);
+    //mulLine(matrix, *cols, 0, (0.5)); addLine(matrix, *cols, 2, 0, 0.2);
+    printf("\n");
+    printMatrix(matrix, *rows, *cols);
 }
