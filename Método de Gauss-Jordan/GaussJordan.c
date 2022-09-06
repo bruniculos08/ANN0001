907440bfde590c87291db5a4eebde698f11cab46
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-09-05 22:20:09 -0300

    update

--- Método de Gauss-Jordan/GaussJordan.c
+++ Método de Gauss-Jordan/GaussJordan.c
@@ -9,6 +9,7 @@ void printMatrix(float **matrix, int rows, int cols){
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 void mulLine(float **matrix, int cols, int resultLine, float mult){
@@ -35,11 +36,15 @@ void swapLine(float **matrix, int cols, int Line1, int Line2){
 // matriz com algum coeficiente 0 não está funcionando (sugestão: fazer função que troque as linhas)
 
 void gaussJordan(float **matrix, int rows, int cols){
-    // (1) A última coluna é a matriz B é tal que A.X = B, ou seja, são os valores que o sistema de equações deve satisfazer.
+    // (1) A última coluna é a matriz B é tal que A.X = B, ou seja, são os valores que o sistema de equações deve satisfazer,...
+    // ... assim, dado que a última coluna da matriz não se trata sobre as variáveis, j varia de 0 a cols-1 pois a última coluna...
+    // ... não é usada no processo de escalonamento:
     for(int j = 0; j < cols-1; j++){
-        for(int i = j; i < rows; i++){
 
-            // (2) Se estivermos em uma linha cujo pivô da atual coluna seria 0, deve-se trocar essa linha com a seguinte,...
+        // (2) i varia de j até rows pois se estamos na coluna j significa que já escalonas a coluna j-1 pela com a linha j-1,...
+        // ... ou seja, devemos agora escalonar para a variável x_j visto que já que escalonamos para x_j-1:
+        for(int i = j; i < rows; i++){
+            // (2.1) Se estivermos em uma linha cujo pivô da atual coluna seria 0, deve-se trocar essa linha com a seguinte,...
             // ... e caso isso também resulte em um pivô nulo, deve-se repetir o processo até que não se possa mais trocar...
             // ... de linhas:
             if(matrix[i][j] == 0 && i < rows-1){
@@ -47,15 +52,17 @@ void gaussJordan(float **matrix, int rows, int cols){
                 while(matrix[k][j] == 0 && k < rows-1) swapLine(matrix, cols, i, i+1);
             }
 
+            // (2.2) Caso tenhamos um pivô de coeficiente diferente de 0 na atual linha i e coluna j, podemos então dividir a...
+            // ... linha deste pivô pelo coeficiente a_ij e então realizar o escalonamento subtraindo a linha i nas outras linhas...
+            // ... sempre multiplicando a linha i pelo coeficiente da outra linha na mesma coluna j ao realizar a subtração:
             if(matrix[i][j] != 0){
-                printf("dividir linha %i (pivo %f)\n", i, matrix[i][j]);
                 divLine(matrix, cols, i, matrix[i][j]);
                 for(int k = 0; k < rows; k++){
                     if(k != i) addLine(matrix, cols, k, i, -matrix[k][j]);
                 }
-                printf("\n");
-                printMatrix(matrix, rows, cols);
-                printf("\n");
+                // (2.3) Caso o escalona mento tenha sido feito com pivô na linha i, deve-se mover para a proxima coluna e...
+                // ... também para a próxima linha (o que é feito pois o for inicia com i=j), para se realizar o escalonamento...
+                // ... para a próxima variável.
                 break;
             }
         }
@@ -67,7 +74,7 @@ float **createMatrix(int *rows, int *cols){
     FILE *filePointer;
     filePointer = fopen("Matrix.txt", "r");
     fscanf(filePointer, "%i %i\n", rows, cols); 
-    printf("%i %i \n", *rows, *cols);
+    printf("\nMatriz de %i linhas e %i colunas. \n\n", *rows, *cols);
 
     // (1) Alocando os ponteiros para as linhas da matriz:
     // - o ponteiro de ponteiro aponta para um espaço a partir de onde há um número rows de ponteiros para int's
@@ -94,7 +101,5 @@ int main(){
     float **matrix = createMatrix(rows, cols);
     printMatrix(matrix, *rows, *cols);
     gaussJordan(matrix, *rows, *cols);
-    //mulLine(matrix, *cols, 0, (0.5)); addLine(matrix, *cols, 2, 0, 0.2);
-    printf("\n");
     printMatrix(matrix, *rows, *cols);
 }
