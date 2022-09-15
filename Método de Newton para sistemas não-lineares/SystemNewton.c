b10170bba61597b8ec294b1a82a618008b9162c7
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-09-15 18:02:53 -0300

    update

--- Método de Newton para sistemas não-lineares/SystemNewton.c
+++ Método de Newton para sistemas não-lineares/SystemNewton.c
@@ -5,27 +5,27 @@
 // Note que as funções são escritas na forma: z = f(x, y)
 
 double f1(double x, double y){
-    return;
+    return x*x +2*(y*y)-3;
 }
 
 double f1x(double x, double y){
-    return;
+    return 2*x;
 }
 
 double f1y(double x, double y){
-    return;
+    return 4*y;
 }
 
 double f2(double x, double y){
-    return;
+    return 4*(x*x) + y*y - 6;
 }
 
 double f2x(double x, double y){
-    return;
+    return 8*x;
 }
 
 double f2y(double x, double y){
-    return;
+    return 2*y;
 }
 
 // A generalização do método de Newton para n variáveis, sendo X_k uma matriz na k-ésima iteração do algoritmo...
@@ -62,7 +62,33 @@ double f2y(double x, double y){
 // (7) [x] = [x0] + [-f(x0, y0)]*[fx(x0, y0), fy(x0, y0)]-¹
 //     [y]   [y0]
 
+// (8) [x] = [x0] - [f(x0, y0)]*[fx(x0, y0), fy(x0, y0)]-¹
+//     [y]   [y0]
 
-double newton(double **matrix){
+// note que é necessário mais de uma equação visto que a matriz [fx(x0, y0), fy(x0, y0)] não é inversível, assim supondo que tivéssimos...
+// ... mais uma equação além desta, teriámos também de ter essa matriz multiplicando do lado esquerdo:
 
+
+// (9) X = X_0 - F(X_0)*[F'(X_0)]-¹
+
+// Eis a fórmula da aproximação de newton para 3 variáveis (e seguindo o mesmo padrão tem-se para n variáveis)
+
+double newton(double x0, double y0, int n){
+    for(int i = 0; i < n; i++){
+        double F[] = {f1(x0, y0), f2(x0, y0)};
+        double det = f1x(x0, y0)*f2y(x0, y0) - f1y(x0, y0)*f2x(x0, y0);
+        double dF_inv[2][2] = {{f2y(x0, y0)/det, -f1y(x0, y0)/det}, {-f2x(x0, y0)/det, f1x(x0, y0)/det}};
+        x0 = x0 - (F[0]*dF_inv[0][0] + F[1]*dF_inv[0][1]);
+        y0 = y0 - (F[0]*dF_inv[1][0] + F[1]*dF_inv[1][1]);
+    }
+    printf("x = %.17f, y = %.17f para %i iteracoes.\n", x0, y0, n);
+}
+
+int main(){
+    int vetor[] = {1, 2, 3, 4, 5};
+    double x0 = 1.3015;
+    double y0 = -1.0469;
+    for(int i = 0; i < sizeof(vetor)/sizeof(vetor[0]); i++){
+        newton(x0, y0, vetor[i]);
+    }
 }
