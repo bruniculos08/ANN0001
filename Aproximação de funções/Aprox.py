d6aec5c1fedaf23c60b0a81d292346fdadf8a9f8
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2022-12-13 21:10:05 -0300

    update

--- Aproximação de funções/Aprox.py
+++ Aproximação de funções/Aprox.py
@@ -16,16 +16,32 @@ def trapeze_sum(f, a, b, n):
 def aprox_coeffs(func_list, f, a, b, n):
     A = []
     B = []
-    for fi in func_list:
+    # Obs.: note que a matriz A é simétrica portanto não precisamos calcular n² integrais
+    for i, fi in enumerate(func_list):
         row = []
         b_i = trapeze_sum(lambda x: f(x)*fi(x), a, b, n)
-        for fj in func_list:
-            # Note que a_ij = ∫ fj(x)*fi(x) dx
-            a_ij = trapeze_sum(lambda x: fi(x)*fj(x), a, b, n)
-            row.append(a_ij)
+        for j, fj in enumerate(func_list):
+            """
+            Note que:
+            (1) a_ij = ∫ fj(x)*fi(x) dx;
+            (2) visto que a matriz A é simétrica e a parte acima da diagonal é calculada primeiro,
+            não é necessário calcular os elementos em que i > j.
+            """
+            if(i <= j):
+                a_ij = trapeze_sum(lambda x: fi(x)*fj(x), a, b, n)
+                row.append(a_ij)
+            else:
+                row.append(A[j][i])
+                
         B.append(b_i)
         A.append(row)
     return np.linalg.solve(A, B)
+
+"""
+Observe que são calculadas n integrais na diagonal + [(n-1) + (n-2) + ... + 2 + 1] integrais das diagonais acima + n integras das integrais
+na matriz B, o que resulta em n(n+1)/2 + n = (n² + 3n)/2 integrais, o que é mesmo assim uma valor O(n²) apesar da otimização em relação aos
+elementos iguais (pela simetria da matriz).
+"""
 
 def build_aprox_func(func_list, coeffs):
     def g(x):
@@ -35,13 +51,14 @@ def build_aprox_func(func_list, coeffs):
 if __name__ == '__main__':
     
     # Exemplo 01:
-    func_list = [lambda x: sin(x), lambda x: cos(x), lambda x: e**x, lambda x: x]
+    func_list = [lambda x: sin(x), lambda x: cos(x), lambda x: sin(3*x), lambda x: cos(3*x), lambda x: sin(5*x), lambda x: cos(5*x), lambda x: 1]
 
     def f(x):
-        return log(x, e) + cos(x) + x
+        if int(x%2) == 0: return 0
+        else: return 1
 
-    a = 1
-    b = 6
+    a = 0
+    b = 10
     n = 256
 
     coeffs = aprox_coeffs(func_list, f, a, b, n)
