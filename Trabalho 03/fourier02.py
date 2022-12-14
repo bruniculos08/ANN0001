216f366729e132bbf28f35f8dc8acb72780a85c6
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2022-12-14 20:52:04 -0300

    update

--- Trabalho 03/fourier02.py
+++ Trabalho 03/fourier02.py
@@ -18,6 +18,22 @@ def richardson(f, a, b, n, k):
 
     return table[0]
 
+def simpson(f, a, b, num_subintervals):
+
+    # Obs.: num_intervals(n) é o número de subintervalos, n/2 é o número de parábolas e n+1 é o número de pontos na partição.
+    h = (b-a)/num_subintervals
+    sum = f(a) + f(b)
+
+    # k varia até 2n
+    for k in range(2, num_subintervals, 2):
+        sum += 2*f(a + k*h)
+
+    # k varia até 2n-1 (quando k = 2n+1 o loop para)
+    for k in range(1, num_subintervals, 2):
+        sum += 4*f(a + k*h)
+
+    sum = (h/3)*sum        
+    return sum
 
 def trapeze_sum(f, a, b, n):
     sum = f(a)/2 + f(b)/2
@@ -36,15 +52,37 @@ def fourier_coeffs(f, a, b, num_coeffs, num_intervals):
     A = []
     B = []
 
-    c = (1/(2*pi))*trapeze_sum(f, a, b, num_intervals)
+    c = (1/(2*pi))*simpson(f, a, b, num_intervals)
 
     for m in range(1, num_coeffs+1):
-        a_m = (1/pi)*trapeze_sum(lambda x: f(x)*cos(m*x), a, b, num_intervals)
-        b_m = (1/pi)*trapeze_sum(lambda x: f(x)*sin(m*x), a, b, num_intervals) 
+        a_m = (1/pi)*simpson(lambda x: f(x)*cos(m*x), a, b, num_intervals)
+        b_m = (1/pi)*simpson(lambda x: f(x)*sin(m*x), a, b, num_intervals) 
         A.append(a_m)
         B.append(b_m)
 
     return A, B, c
+
+# Usar essa função para questões do tipo:
+def quadrature(f, a, b, cord, coeffs):
+
+    g = change(f, a, b)
+    
+    sum = 0
+    for xi, ci in zip(cord, coeffs):
+        sum += ci*g(xi)
+    return sum
+
+# Transforma string em função:
+def stringToFunc(string):
+    def f(x):
+        return eval(string)
+    return f
+
+# Mudança de variável na função f para se encaixar nos limites de integração [-1, 1]:
+def change(f, a, b):
+    def g(u):
+        return f((b+a)/2 + (b-a) * (u/2)) * (b-a)/2
+    return g
 
 def build_fourier(A, B, c):
     """
@@ -58,10 +96,10 @@ def build_fourier(A, B, c):
 if __name__ == "__main__":
     
     def f(x):
-        return x * sin(6*e**(-x**2))
+        return x * sin(10 * x**2 * exp(-x**2))
 
-    num_intervals = 1024
-    num_coeffs = 5
+    num_intervals = 256
+    num_coeffs = 10
     a = -pi
     b = pi
 
@@ -72,16 +110,23 @@ if __name__ == "__main__":
         print(f"{a_n}, {b_n},")
     
     g = build_fourier(A, B, c)
-    values = [-2.463, 0.665, 2]
+    values = [-2.111, 0.349, 2.343]
     for i, xi in enumerate(values):
         print(f"g(x_{i+1}) = {g(xi)}")
 
-    k = 8
-    num_intervals = 10 
-    erro = richardson(lambda x: (f(x)-g(x))**2, a, b, num_intervals, k)
+    # Usando método da quadratura gaussiana:
+    from NodesAndWeights import *
+
+    exact_for_degree_less_than = 24
+    order = str(int(exact_for_degree_less_than/2))    
+    lists_names = ['raiz'+order, 'peso'+order]
+    
+    cord = locals()[lists_names[0]]
+    coeffs = locals()[lists_names[1]]
+
+    erro = quadrature(lambda x: (f(x)-g(x))**2, a, b, cord, coeffs)
     print(erro)
     # Lembre-se que num_intervals = (b-a)/h
-
 
     t = np.linspace(-pi, pi, 200)
     ft = [f(ti) for ti in t]
@@ -90,5 +135,5 @@ if __name__ == "__main__":
     plt.plot(t, ft, color='green', label='f(x)')
     plt.plot(t, gt, color='blue', label='Fourier Serie for f(x)')
     plt.legend()
-    plt.savefig("fourier01.png")
+    plt.savefig("fourier02.png")
     plt.close()
